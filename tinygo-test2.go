fc7627466f3ba6801da0c980c11c224a0a4226d8
Will Clingan <wuz352@hotmail.com>
2021-03-23 12:16:19 -0500

    Update tinygo-test2.go

--- tinygo-test2.go
+++ tinygo-test2.go
@@ -1,46 +1,46 @@
 package main
 
 import (
-    "machine"
-    "time"
+	"machine"
+	"time"
 )
 
 func main() {
-    led := machine.LED
-    led.Configure(machine.PinConfig{Mode: machine.PinOutput})
-    num := 0
-    timing := func() {
-        time.Sleep(time.Millisecond * 250)
-    }
-    for {
-        if (num % 2) == 0 {
-            led.Low()
-            timing()
-        
-            led.High()
-            time.Sleep(time.Millisecond * 200)
-        
-            led.Low()
-            time.Sleep(time.Millisecond * 100)
-        
-            led.High()
-            time.Sleep(time.Millisecond * 200)
-        
-            led.Low()
-            timing()
-            if (num == 100) {
-                num = 0
-            }
-        } else {
-            led.Low()
-            timing()
-        
-            led.High()
-            time.Sleep(time.Millisecond * 500)
-        
-            led.Low()
-            timing()
-        }
-        num++
-    }
+	led := machine.LED
+	led.Configure(machine.PinConfig{Mode: machine.PinOutput})
+	num := 0
+	timing := func() {
+		<-time.Tick(time.Millisecond * 250)
+	}
+	for {
+		if (num % 2) == 0 {
+			led.Low()
+			timing()
+
+			led.High()
+			<-time.Tick(time.Millisecond * 200)
+
+			led.Low()
+			<-time.Tick(time.Millisecond * 100)
+
+			led.High()
+			<-time.Tick(time.Millisecond * 200)
+
+			led.Low()
+			timing()
+			if num == 100 {
+				num = 0
+			}
+		} else {
+			led.Low()
+			timing()
+
+			led.High()
+			<-time.Tick(time.Millisecond * 500)
+
+			led.Low()
+			timing()
+		}
+		num++
+	}
 }
