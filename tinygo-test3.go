54fbd9fe945f987e07a8e5a207caf2d394408114
Will Clingan <wuz352@hotmail.com>
2021-03-23 12:40:50 -0500

    Update tinygo-test3.go

--- tinygo-test3.go
+++ tinygo-test3.go
@@ -1,51 +1,55 @@
 package main
 
 import (
-    "machine"
-    "time"
-    "sync"
+	"machine"
+	"sync"
+	"time"
 )
 
+/*
+  CONCURRENT LED
+*/
+
 func main() {
-    led := machine.LED
-    led.Configure(machine.PinConfig{Mode: machine.PinOutput})
+	led := machine.LED
+	led.Configure(machine.PinConfig{Mode: machine.PinOutput})
 
-    wg := &sync.WaitGroup{}
-    wg.Add(2)
+	wg := &sync.WaitGroup{}
+	wg.Add(2)
 
-    go func() {
-        defer wg.Done()
-        for i := 0; i < 100; i++ {
-            led.Low()
-            time.Sleep(time.Millisecond * 250)
+	go func() {
+		defer wg.Done()
+		for i := 0; i < 100; i++ {
+			led.Low()
+			<-time.Tick(time.Millisecond * 250)
 
-            led.High()
-            time.Sleep(time.Millisecond * 500)
+			led.High()
+			<-time.Tick(time.Millisecond * 5000)
 
-            led.Low()
-            time.Sleep(time.Millisecond * 250)
-        }
-    }()
+			led.Low()
+			<-time.Tick(time.Millisecond * 250)
+		}
+	}()
 
-    go func() {
-        defer wg.Done()
-        for j := 0; j < 100; j++ {
+	go func() {
+		defer wg.Done()
+		for j := 0; j < 100; j++ {
 
-            led.Low()
-            time.Sleep(time.Millisecond * 250)
+			led.Low()
+			<-time.Tick(time.Millisecond * 250)
 
-            led.High()
-            time.Sleep(time.Millisecond * 200)
+			led.High()
+			<-time.Tick(time.Millisecond * 200)
 
-            led.Low()
-            time.Sleep(time.Millisecond * 100)
+			led.Low()
+			<-time.Tick(time.Millisecond * 100)
 
-            led.High()
-            time.Sleep(time.Millisecond * 200)
+			led.High()
+			<-time.Tick(time.Millisecond * 200)
 
-            led.Low()
-            time.Sleep(time.Millisecond * 250)
-        }
-    }()
-    wg.Wait()
+			led.Low()
+			<-time.Tick(time.Millisecond * 250)
+		}
+	}()
+	wg.Wait()
 }
